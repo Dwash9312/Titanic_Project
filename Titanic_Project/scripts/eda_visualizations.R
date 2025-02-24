b6adcf5d8c0eafc23e5f546e815e13ddf012caf4
Dwash9312 <134456721+Dwash9312@users.noreply.github.com>
2025-02-23 19:37:08 -0600

    Delete Titanic_Project directory

--- Titanic_Project/scripts/eda_visualizations.R
+++
@@ -1,7 +0,0 @@
-#Visualizing Survival by Gender
-ggplot(train, aes(x = Survived, fill = Sex)) +
-  geom_bar() +
-  labs(title = "Survival by Gender")
-# Correlation Analysis
-numeric_vars <- train %>% select(where(is.numeric))
-corrplot(cor(numeric_vars, use = "complete.obs"), method = "circle")
--- Titanic_Project/scripts/model_training.R
+++
@@ -1,7 +0,0 @@
-#Model Training
-set.seed(42)
-train_index <- createDataPartition(train$Survived, p = 0.8, list = FALSE)
-train_data <- train[train_index, ]
-test_data <- train[-train_index, ]
-model <- rpart(Survived ~ Pclass + Sex + Age + Fare + FamilySize, data = train_data, method = "class")
-rpart.plot(model)
